d184fb6bd6bc62c3a546382bbf32881e156bb0a0
Kannu Mehta <kannumehta@kannus-air.lan>
2022-05-01 09:33:49 -0700

    fixed measure_iops.sh

--- measure_iops.sh
+++ measure_iops.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 sudo apt-get install fio
-fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75 followed by fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=150G --readwrite=randrw --rwmixread=75
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=75
+fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=test --bs=4k --iodepth=64 --size=150G --readwrite=randrw --rwmixread=75
